b86894ba7fce737c8d032c78b57a9b2164590f68
DP051767 <Dhruv.Patel@cerner.com>
2017-09-22 15:00:25 -0500

    BinarySearch in go language

---
+++ Go/BinarySearch.go
@@ -0,0 +1,39 @@
+package main
+import "fmt"
+
+func isNumberPresent(num int, arrayOfNumbers[] int, length int) bool{
+	var low, high = 0, length - 1
+	var mid int;
+	var result bool;
+	
+	 for high >= low{
+		mid = (low+high) / 2
+
+		if arrayOfNumbers[mid] == num{
+			result = true
+			break;
+		}
+		
+		if arrayOfNumbers[mid] < num{
+			low = mid + 1
+		}
+		
+		if arrayOfNumbers[mid] > num{
+			high = mid - 1
+		}
+	}
+	
+	if (result == true){
+		return result
+	}else{
+		return false
+	}
+}
+
+func main() {  
+  var num int = 5
+  var arrayOfNumbers = []int {2,5,7,8,9}
+  var output bool
+  output = isNumberPresent(num, arrayOfNumbers, 5)
+  fmt.Printf("True if number presnt otherwise false: %v\n", output )
+}
